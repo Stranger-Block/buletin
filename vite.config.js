4403bfba150c859d6204d3a46dfbb83325dcd0bb
Stranger-Block <blockchaindevloper2202@gmail.com>
2023-11-22 08:25:06 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/News/",
+  base: '/News/',
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: ['@fortawesome/react-fontawesome'],
+    },
+  },
+});
+
